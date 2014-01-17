e9a3bd843c9f43aad8cc04781fbfdc27fbe9104f
Darren M <aeneascorrupt@gmail.com>
2014-01-17 00:42:18 -0800

    test #6

--- test/index.func.test.js
+++ test/index.func.test.js
@@ -7,19 +7,19 @@ Y.TestRunner.add(new Y.TestCase({
     "name": "Functional Test Index",
 
     "setUp": function () {
+        this.app = require('../index');
         this.featureFile = helper.generateTestFile();
     },
 
     "tearDown": function () {
         helper.destroyTestFile(this.featureFile);
-        this.watcher.close();
     },
 
     "load a set of feature flags from file": function () {
         var me = this,
             testFeatures = helper.loadDataFromFile(me.featureFile);
 
-        me.watcher = me.app.load(me.featureFile, function () {
+        me.app.load(me.featureFile, function () {
             me.resume(function () {
                 Object.keys(testFeatures).forEach(function (feature) {
                     if (testFeatures.hasOwnProperty(feature)) {
